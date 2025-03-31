8ef9ca2682b4c2bcc2b3dad3b34613ab398c5821
roblevay <108578793+roblevay@users.noreply.github.com>
2025-03-31 19:29:17 +0200

    Delete Mod08 directory

--- Mod08/TRANCOUNT.sql
+++
@@ -1,8 +0,0 @@
-SELECT @@TRANCOUNT--Antal aktiva transaktioner
-
-BEGIN TRAN--@@TRANCOUNT ökar med 1
-BEGIN TRAN--@@TRANCOUNT ökar med 1
-
-COMMIT TRAN--@@TRANCOUNT minskar med 1
-
-ROLLBACK TRAN--@@TRANCOUNT blir 0
